127a51e85c48e8ae6b8e46d6fcf42cc33c509199
NouR <NouR@Microsoft>
2018-01-24 00:24:47 +0200

    First Commit (Software Project By Aya and Nour)

---
+++ src/Aya_NouR.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author NouR Abu Hijleh & Aya Shanaa
+ *
+ */
+public class Aya_NouR {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String st= "Welcome to the starting of our software learning process\n\n" +"\t   By: Nour Abu Hijleh & Aya Shanaa"+
+				"\n\n\t Teacher Supervisor: Dr. Haya Samaana";
+		System.out.printf(st);
+
+	}
+
+}
